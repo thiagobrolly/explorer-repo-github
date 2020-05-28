006521ba8b0297f87e776549442764e295b0e7d2
Thiago Brolly <thiagoex377@gmail.com>
2020-05-28 12:02:51 -0300

    Projeto com Typescript

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,95 @@
+import React, {useState, useEffect, FormEvent} from 'react';
+import {FiChevronRight} from 'react-icons/fi';
+import {Link} from 'react-router-dom';
+import api from '../../services/api';
+
+import logoImg from '../../assets/logo.svg';
+
+import {Title, Form, Repositories, Error} from './styles';
+
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
+const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [inputError, setInputError] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>(() => {
+    const storageRepositories = localStorage.getItem(
+      '@GithubExplorer:repositories',
+    );
+
+    if (storageRepositories) {
+      return JSON.parse(storageRepositories);
+    }
+    return [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem(
+      '@GithubExplorer:repositories', JSON.stringify(repositories),
+    );
+  }, [repositories]);
+
+  async function addRepository(e: FormEvent<HTMLFormElement>): Promise<void>{
+    e.preventDefault();
+
+    if(!newRepo){
+      setInputError('Digite o autor/nome do repositório');
+      return;
+    }
+
+    try{
+      const response = await api.get<Repository>(`repos/${newRepo}`);
+
+      const repository = response.data;
+
+      setRepositories([...repositories, repository]);
+      setInputError('');
+      setNewRepo('');
+    } catch(err){
+      setInputError('Repositório não encontrado.');
+    }
+  }
+
+  return(
+    <>
+      <img src={logoImg} alt='Github Explorer' />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form hasError={!! inputError} onSubmit={addRepository}>
+        <input
+          value={newRepo}
+          onChange={(e) => setNewRepo(e.target.value)}
+          placeholder='Digite o nome do repositório'
+        />
+        <button type='submit'>Pesquisar</button>
+      </Form>
+
+      {/* Condicional */}
+      { inputError && <Error>{inputError}</Error> }
+
+      <Repositories>
+        {repositories.map((repository) => (
+          <Link key={repository.full_name} to={`/repository/${repository.full_name}`}>
+            <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+
+            <FiChevronRight size={20} />
+          </Link>
+        ))}
+      </Repositories>
+    </>
+  );
+}
+
+export default Dashboard;
---
+++ src/routes/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import Dashboard from '../pages/Dashboard';
+import Repository from '../pages/Repository';
+
+const Routes: React.FC = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path='/' component={Dashboard} />
+      <Route path='/repository/:repository+' component={Repository} />
+    </Switch>
+  </BrowserRouter>
+);
+
+export default Routes;
